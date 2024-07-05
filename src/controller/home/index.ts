82e997b5a65547f34803c97f1f5568cdfe34a3c8
millent <millent@126.com>
2024-07-05 09:10:32 +0800

    re change

--- src/controller/home/index.ts
+++ src/controller/home/index.ts
@@ -2,21 +2,6 @@ import { AppController } from "@/mixin/appController";
 import {ApiDescription, Context, Get, Request} from "fire-cat";
 import schema from "./schema";
 import { AppVerify } from "@/decorators/appVerify";
-const fs = require('fs');
-const path = require('path');
-
-// 读取文件内容的函数
-async function readFileContent(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-}
 
 export class HomeController extends AppController {
 
@@ -29,17 +14,10 @@ export class HomeController extends AppController {
 
   @Get('/ping')
   @Request()
-  // @AppVerify(schema.test)
+  @AppVerify(schema.test)
   @ApiDescription('ping的页面')
   async ping(ctx: Context) {
-    // ctx.body = 'pang'
-
-    console.log({
-      1: path.join(__dirname, '/public/test.txt'),
-      2: __dirname
-    })
-    const content = await readFileContent(path.join(__dirname, '../../public/test.txt'));
-    ctx.body = content;
+    ctx.body = 'pang'
   }
 
 }
